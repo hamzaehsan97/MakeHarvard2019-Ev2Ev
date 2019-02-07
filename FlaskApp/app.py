9e258b031a438d7d02e6d6add12502f3cf8755e1
Hamza Ehsan <hamza.ehsan@lawrence.edu>
2019-02-06 22:02:07 -0600

    added basic flask app updates

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from flask import render_template
+from data import  Locations
+from flask import request
+
 
 app = Flask(__name__)
+
+Locations = Locations()
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/dataTest', methods = ['POST','GET'])
+def dataTest():
+    if request.method == 'POST':
+        dataTest = request.form
+        return render_template('dataTest.html', dataTest=dataTest)
 
-@app.route('/get_locations')
-def index2():
-    return render_template('get_locations.html')
+
+@app.route('/locations')
+def locations():
+    return render_template('locations.html', Locations = Locations)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ FlaskApp/data.py
@@ -0,0 +1,22 @@
+def Locations():
+    locations = [
+        {
+        'id' : 1,
+        'name' : 'Hamza',
+        'latitude' : 10.000,
+        'longitude' : 10.000
+        },
+        {
+        'id' : 2,
+        'name' : 'Huzaifa',
+        'latitude' : 110.000,
+        'longitude' : 110.000
+        },
+        {
+        'id' : 3,
+        'name' : 'Talha',
+        'latitude' : 120.000,
+        'longitude' : 130.000
+        }
+    ]
+    return locations
